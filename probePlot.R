fd31d8381f6d81642b251298d493ed8c72b88da0
Jacob Fredsoe <fredsoee@hotmail.com>
2016-06-07 13:58:44 +0200

    Moved stuff around

--- probePlot.R
+++ probePlot.R
@@ -1,30 +1,3 @@
-
-if(!exists("betaData")) {
-  betaData = readRDS("O:/HE_MOMA-Data/MICROARRAY/Prostata/450K_MarmalAid/Scripts/output.rds")
-}
-if(!exists("betaDataSets")) {
-  betaDataSets = readRDS("O:/HE_MOMA-Data/MICROARRAY/Prostata/450K_MarmalAid/Scripts/outputSets.rds")
-}
-
-#betaDataSets = list(setNames = betaDataSets[[1]], columnNames = betaDataSets[[2]])
-
-probes = rownames(betaData)[grep("GABRE", betaData$GENE_BY_JBBR)]
-
-columnSelect = intersectR(c(grep("allcancer", as.vector(betaDataSets$setNames), ignore.case = TRUE), grep("allhealthy", as.vector(betaDataSets$setNames), ignore.case = TRUE)), grep("%>=0.6", as.vector(betaDataSets$columnNames)))
-
-betaDataVar = betaData[order(apply(betaData[,columnSelect], 1, var), decreasing = TRUE),]
-
-probePlot(rownames(betaDataVar)[1:4], betaData, betaDataSets, 6, plotType = "lineplot")
-
-require(manipulate)
-
-manipulate({
-  probePlot(rownames(betaDataVar)[1:9], betaData, betaDataSets, betaValue)
-}, betaValue = slider(0,10))
-
-
-
-
 probePlot = function(probes, betaData, betaDataSets, betaCut = NULL, plotType = "boxplot") {
   require(ggplot2)
   require(reshape2)
@@ -53,51 +26,6 @@ probePlot = function(probes, betaData, betaDataSets, betaCut = NULL, plotType = 
       xlab("Dataset") + ylab("Percentage")
   }
   if(plotType == "lineplot") {
-    # Multiple plot function
-    #
-    # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-    # - cols:   Number of columns in layout
-    # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-    #
-    # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-    # then plot 1 will go in the upper left, 2 will go in the upper right, and
-    # 3 will go all the way across the bottom.
-    #
-    multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-      require(grid)
-      
-      # Make a list from the ... arguments and plotlist
-      plots <- c(list(...), plotlist)
-      
-      numPlots = length(plots)
-      
-      # If layout is NULL, then use 'cols' to determine layout
-      if (is.null(layout)) {
-        # Make the panel
-        # ncol: Number of columns of plots
-        # nrow: Number of rows needed, calculated from # of cols
-        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                         ncol = cols, nrow = ceiling(numPlots/cols))
-      }
-      
-      if (numPlots==1) {
-        print(plots[[1]])
-        
-      } else {
-        # Set up the page
-        grid.newpage()
-        pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-        
-        # Make each plot, in the correct location
-        for (i in 1:numPlots) {
-          # Get the i,j matrix positions of the regions that contain this subplot
-          matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-          
-          print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                          layout.pos.col = matchidx$col))
-        }
-      }
-    }
     
     valuePicker = which(columnNames %in% percentageNames)
     
@@ -121,7 +49,6 @@ probePlot = function(probes, betaData, betaDataSets, betaCut = NULL, plotType = 
     }
     
     g = multiplot(plotlist = gplots, cols = sqrt(length(unique(graphData$ProbeID))))
-    
   }
   return(g)
 }
